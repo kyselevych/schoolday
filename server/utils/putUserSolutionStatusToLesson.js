97281c3ae240862667a00dc292be1e8008bb19d1
Volodymyr Kyselevych <wilkarich@gmail.com>
2022-04-10 23:05:59 +0300

    Edited: server function getting status of lesson

--- server/utils/putUserSolutionStatusToLesson.js
+++ server/utils/putUserSolutionStatusToLesson.js
@@ -1,12 +1,12 @@
 function putUserSolutionStatusToLesson(lessonsList, solutionsList) {
 	for (let iLesson = 0; iLesson < lessonsList.length; iLesson++) {
+		lessonsList[iLesson].dataValues.status = 'unsent';
+		
 		for (let iSolution = 0; iSolution < solutionsList.length; iSolution++) {
 			if (lessonsList[iLesson].id === solutionsList[iSolution].lessonId) {
 				lessonsList[iLesson].dataValues.status = solutionsList[iSolution].status;
 				break;
 			}
-			
-			lessonsList[iLesson].dataValues.status = 'unsent';
 		}
 	}
 }
